c4da3db74ac152e913b0e22bd12104977697b70a
AlejandroMunozA <71931323+AlejandroMunozA@users.noreply.github.com>
2020-12-19 17:10:29 -0300

    r

---
+++ Trabajo_BigData.R
@@ -0,0 +1,39 @@
+###########################Instrucciones Trabajo Final###########################
+
+#Antes de comenzar crearán un repositorio en Git para la entrega del trabajo final, el cual será llamado “Trabajo_BigData” .
+#Los pasos a seguir, serán clonar el repositorio previamente creado en su equipo local.
+#Configure el repositorio descargado, como su espacio de trabajo en el RStudio. 
+
+
+#El plazo de entrega será hasta el 4 de Enero a las 23:59
+#enviar el link del git a los correos diana.lopez@utem.cl y amaru.fernandezd@utem.cl .
+
+########################### Se les pide lo siguiente: ###########################
+
+########################### A) Cree una presentación (PPT) que tenga como estructura ( 10 ptos ): ###########################
+
+#-Portada: Nombre de los autores, título del trabajo, nombre profesores de cátedra.
+#-Introducción: Antecedentes y motivación del estudio
+#-Objetivos que se plantearon, los cuales se establecieron en la primera tarea
+#-Metodología: Donde expliquen y muestren donde encontraron la clase, tag, id etc de sus datos. 
+#Esto a grandes rasgos, podrían agregar otra cosa que les parece #relevante para complementar
+#-Resultados: Que exponga estadística descriptiva de los datos y algunos gráficos
+#que permitan un análisis más profundo.
+#-Conclusiones y comentarios finales.
+#No deben ser más de 10 slides, donde lo señalado anteriormente es lo mínimo a contener.
+
+########################### B) Script que contenga (20 ptos): ###########################
+
+#-Desarrollo de la extracción de datos
+#-Desarrollo de los gráficos
+#-Comentarios de los procesos
+#-Pequeños análisis de los datos
+#Recordar ser ordenado, cuidar redacción y ortografía , ya que será penalizado en el caso de tener faltas.
+
+########################### C) Un video de 15 min máximo (10 ptos). ###########################
+
+#donde muestre y explique sus resultados. En definitiva, presente la ppt que hizo en la letra A. 
+
+############################ D) Las bases de datos que recopiló (5 ptos)###########################
+
+#las cuales deben contener una pequeña descripción de sus columnas.
